fcd1c141741815c754c82de861ce70c1e86709c6
Balagnar <silvagabrielpinheiro@gmail.com>
2026-04-24 15:30:38 -0400

    feat:setup inicial da v0.5.0 com estrutura de assets corrigida.

---
+++ src/Arena.java
@@ -0,0 +1,45 @@
+public class Arena {
+
+    //Indicador de versão
+    public static final String VERSION = "0.5.0-Alpha";
+
+    //Variaveis de instância
+    private Guerreiro heroi;
+    private Inimigo slime;
+
+    //Metodo para criar os objetos
+    public Arena() {
+        this.heroi = new Guerreiro("Heroi", 20, 10);
+        this.slime = new Inimigo("Slime", 20, 10);
+        this.slime.spawn(this.slime);
+    }
+
+    //Metodo responsavel pela logica do ataque
+    public void turnoDeAtaque() {
+        if (heroi.getVida() > 0 && slime.getVida() > 0) {
+            heroi.darDano(slime);
+            
+            // O ataque do slime deve estar dentro do IF de vida
+            if (slime.getVida() > 0) {
+                slime.darDano(heroi);
+            }
+        }
+    }
+
+    //Metodos para o MainApp saber os dados dessas variaveis já que são privadas
+    public int getVidaHeroi() {
+        return heroi.getVida();
+    }
+
+    public int getVidaSlime() {
+        return slime.getVida();
+    }
+
+    public String getNomeHeroi() {
+        return heroi.getNome();
+    }
+
+    public String getNomeSlime() {
+        return slime.getNome();
+    }
+}
---
+++ src/Guerreiro.java
@@ -0,0 +1,74 @@
+import java.util.Random;
+
+public class Guerreiro{
+
+    private String nome;
+    private int vida;
+    private int dano;
+
+
+    public Guerreiro(String nome, int vida, int dano){
+        this.nome = nome;
+        this.vida = vida;
+        this.dano = dano;
+    }
+
+    public void setNome(String nome){
+        this.nome = nome;
+    }
+
+    public String getNome(){
+        return this.nome;
+    }
+
+    public void setVida(int vida){
+        this.vida = vida;
+    }
+
+    public int getVida(){
+        return this.vida;
+    }
+
+    public void setDano(int dano){
+        this.dano = dano;
+    }
+
+    public int getDano(){
+        return this.dano;
+    }
+
+    public void tomarDano(int danoInimigo){
+
+        Random rngDodge = new Random();
+
+          if(rngDodge.nextInt(10) >= 8){
+            System.out.println("Guerreiro: " + this.nome + " Desviou do ataque");
+        } else {
+            this.vida -= danoInimigo;
+            System.out.println("Guerreiro: " + this.nome + " Não desviou");
+        }
+
+    }
+
+    //classe para dar dano
+    public void darDano(Inimigo slime){
+
+        Random gerador = new Random();
+
+        if (gerador.nextInt(10) >= 5) {
+
+            int novaVida = slime.getVida() - this.dano; //"novaVida" recebe o valor da vida atual - dano
+            slime.setVida(novaVida); //"nova vida" seta valor na vida do alvo.
+            System.out.println("Guerreiro: " + " " + this.nome + " atacou o " + slime.getNome() + "com: " + this.dano + " de dano");
+            System.out.println("Inimigo: " + slime.getNome() + " tomou dano, vida atual: " + slime.getVida() + " de vida:");
+  
+            
+        } else {
+            System.out.println("Guerreiro: " + this.nome + " Errou o ataque!");
+        }
+
+        
+        
+    }
+}
+
